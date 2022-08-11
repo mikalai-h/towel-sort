be17e1e79c4cedab5ea035cc5789da335ab8c86e
Hetsman Mikalai <101629314+mikalai-h@users.noreply.github.com>
2022-08-11 19:45:19 +0300

    feat: solved

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 module.exports = function towelSort (matrix) {
-
-   if(matrix == undefined) return []
+   
    for (let i = 0; i < matrix.length; i++) {
      if(i%2) matrix[i].reverse();
    }
    return matrix.flat()
+
 }
